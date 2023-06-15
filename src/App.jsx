8a1378956b2c780252ecc6c50f0dd1f1742e2390
Arthur Soares <arthur.mts@gmail.com>
2023-06-15 00:04:01 -0300

    ajeitando o design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
-import {IconContext} from 'react-icons'
-import {BsBalloon} from 'react-icons/bs'
+import { IconContext } from 'react-icons'
+import { BsBalloon } from 'react-icons/bs'
 import ballon from './assets/ballon.svg'
 import close from './assets/close.svg'
 import menuHamburger from './assets/menu_hamburger.svg'
@@ -15,7 +15,7 @@ function Ballon({ id, color }) {
         <p className='mb-4'>{id}</p>
         <IconContext.Provider value={{ color: color, size: "5rem", className: `global-class-name drop-shadow-[0_0px_10px_${color}]` }}>
           <div>
-            <BsBalloon/>
+            <BsBalloon />
           </div>
         </IconContext.Provider>
       </div>
@@ -44,7 +44,7 @@ function App() {
 
 
   const udpateBaloonEffectOnAPI = useCallback(async (effect) => {
-    console.log("Enviando: "+ effect)
+    console.log("Enviando: " + effect)
     await updateBaloonEffect(effect);
     await new Promise((resolve) => {
       setTimeout(() => {
@@ -61,26 +61,11 @@ function App() {
   return (
     <>
 
-      <div className="h-screen grid grid-cols-3 max-sm:grid-cols-1 transition">
-        <div onClick={handleClickToggleMenu} className={`${isActive ? 'hidden' : null} transition sm:hidden fixed right-10 top-10 cursor-pointer z-10`}><img src={menuHamburger} /></div>
-        <div onClick={handleClickToggleMenu} className={`${isActive ? null : 'hidden'} transition sm:hidden fixed right-10 top-10 cursor-pointer invert z-10`}><img src={close} /></div>
-        <div className='col-span-2 flex flex-col  gap-10 justify-center items-center'>
-          <h1 className='text-3xl max-xs:py-10'>Balões</h1>
-          <div className='flex flex-wrap justify-center items-center gap-10'> 
-            {
-              baloonQuantity ? Array(baloonQuantity).fill(true).map((_, i) => <Ballon key={i} id={i + 1} color={ staticEffectsNames[baloonEffect.toString()] } />) : <Spinner />
-            }
-          </div>
-        </div>
-        <div className={`${isActive ? 'max-sm:-right-px' : ''} bg-black col-span-1 flex justify-center items-start 
-      max-sm:fixed max-sm:-right-full max-sm:h-screen transition-all`}>
-
-
-          <form action="" //Menu de seleção de efeito baloonEffect.toString()
-            className='flex flex-col justify-center items-center'>
-            <div className='mt-6 px-6 max-sm:mt-28'>
-              <label htmlFor="cars" className='text-white'>Escolha um efeito: </label>
-              <select className='bg-white' id="cars" value={baloonEffect || "1"}
+      <div className="h-screen grid grid-rows-2 transition mx-28">
+      <form action="" className={`${isActive ? 'max-sm:-right-px' : ''} rounded-xl mt-10 h-3/4	bg-black max-sm:fixed max-sm:-right-full max-sm:h-screen transition-all`}>
+            <div className='mt-20 ml-16 pr-16 flex flex-col	max-w-md '>
+              <label htmlFor="effects" className='text-white text-4xl'>Adicione efeitos em todos os balões: </label>
+              <select className='mt-8 rounded-md text-center py-1 bg-white text-base' id="effects" value={baloonEffect || "1"}
                 onChange={(e) => {
                   setBaloonEffect(e.target.value)
                 }}
@@ -96,15 +81,24 @@ function App() {
                   ))}
                 </optgroup>
               </select>
-            </div>
-            <div>
               <button type='submit' onClick={(e) => {
                 e.preventDefault();
                 udpateBaloonEffectOnAPI(baloonEffect);
               }}
-                className='bg-white px-2 py-1 mt-4 rounded'>Salvar</button>
+                className='text-sm bg-white py-1 mt-4 rounded-md w-3/12 self-end mr-2'>Adicionar</button>
             </div>
-          </form>
+        </form>
+
+        
+        <div onClick={handleClickToggleMenu} className={`${isActive ? 'hidden' : null} transition sm:hidden fixed right-10 top-10 cursor-pointer z-10`}><img src={menuHamburger} /></div>
+        <div onClick={handleClickToggleMenu} className={`${isActive ? null : 'hidden'} transition sm:hidden fixed right-10 top-10 cursor-pointer invert z-10`}><img src={close} /></div>
+        <div className='flex flex-col gap-10 justify-center items-center'>
+          <h1 className='text-3xl max-xs:py-10'>Balões</h1>
+          <div className='flex flex-wrap justify-center items-center gap-10'>
+            {
+              baloonQuantity ? Array(baloonQuantity).fill(true).map((_, i) => <Ballon key={i} id={i + 1} color={staticEffectsNames[baloonEffect.toString()]} />) : <Spinner />
+            }
+          </div>
         </div>
       </div>
     </>
